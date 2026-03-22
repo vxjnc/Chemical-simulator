e157201abceca12bfc74a38a63a82817707c6e01
ElectroZybr <electrozybr@gmail.com>
2026-03-22 20:53:55 +0300

    feat: добавлены геттеры и сеттеры в AtomStorage
    (cherry picked from commit f9d41b74b6bd1223afd8494e52af01933a4a2574)

--- Engine/physics/AtomStorage.h
+++ Engine/physics/AtomStorage.h
@@ -140,4 +140,83 @@ class AtomStorage {
             valence.pop_back();
             isFixed.pop_back();
         }
+
+        float& posX(std::size_t i) { return x[i]; }
+        float& posY(std::size_t i) { return y[i]; }
+        float& posZ(std::size_t i) { return z[i]; }
+        const float& posX(std::size_t i) const { return x[i]; }
+        const float& posY(std::size_t i) const { return y[i]; }
+        const float& posZ(std::size_t i) const { return z[i]; }
+
+        float& velX(std::size_t i) { return vx[i]; }
+        float& velY(std::size_t i) { return vy[i]; }
+        float& velZ(std::size_t i) { return vz[i]; }
+        const float& velX(std::size_t i) const { return vx[i]; }
+        const float& velY(std::size_t i) const { return vy[i]; }
+        const float& velZ(std::size_t i) const { return vz[i]; }
+
+        float& forceX(std::size_t i) { return fx[i]; }
+        float& forceY(std::size_t i) { return fy[i]; }
+        float& forceZ(std::size_t i) { return fz[i]; }
+        const float& forceX(std::size_t i) const { return fx[i]; }
+        const float& forceY(std::size_t i) const { return fy[i]; }
+        const float& forceZ(std::size_t i) const { return fz[i]; }
+
+        float& prevForceX(std::size_t i) { return pfx[i]; }
+        float& prevForceY(std::size_t i) { return pfy[i]; }
+        float& prevForceZ(std::size_t i) { return pfz[i]; }
+        const float& prevForceX(std::size_t i) const { return pfx[i]; }
+        const float& prevForceY(std::size_t i) const { return pfy[i]; }
+        const float& prevForceZ(std::size_t i) const { return pfz[i]; }
+
+        Atom::Type& type(std::size_t i) { return atomType[i]; }
+        const Atom::Type& type(std::size_t i) const { return atomType[i]; }
+
+        float& energy(std::size_t i) { return potential_energy[i]; }
+        const float& energy(std::size_t i) const { return potential_energy[i]; }
+
+        int& valenceCount(std::size_t i) { return valence[i]; }
+        const int& valenceCount(std::size_t i) const { return valence[i]; }
+        
+        bool isAtomFixed(std::size_t i) { return isFixed[i]; }
+
+        Vec3D pos(std::size_t i) const {
+            return Vec3D(x[i], y[i], z[i]);
+        }
+
+        Vec3D vel(std::size_t i) const {
+            return Vec3D(vx[i], vy[i], vz[i]);
+        }
+
+        Vec3D force(std::size_t i) const {
+            return Vec3D(fx[i], fy[i], fz[i]);
+        }
+
+        Vec3D prevForce(std::size_t i) const {
+            return Vec3D(pfx[i], pfy[i], pfz[i]);
+        }
+
+        void setPos(std::size_t i, const Vec3D& coords) {
+            x[i] = static_cast<float>(coords.x);
+            y[i] = static_cast<float>(coords.y);
+            z[i] = static_cast<float>(coords.z);
+        }
+
+        void setVel(std::size_t i, const Vec3D& speed) {
+            vx[i] = static_cast<float>(speed.x);
+            vy[i] = static_cast<float>(speed.y);
+            vz[i] = static_cast<float>(speed.z);
+        }
+
+        void setForce(std::size_t i, const Vec3D& force) {
+            fx[i] = static_cast<float>(force.x);
+            fy[i] = static_cast<float>(force.y);
+            fz[i] = static_cast<float>(force.z);
+        }
+
+        void setPrevForce(std::size_t i, const Vec3D& prev_force) {
+            pfx[i] = static_cast<float>(prev_force.x);
+            pfy[i] = static_cast<float>(prev_force.y);
+            pfz[i] = static_cast<float>(prev_force.z);
+        }
 };
