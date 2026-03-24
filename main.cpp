8ebf4954ca9e8d66f412809ede619263576f657a
vxjnc <vxjnc0@gmail.com>
2026-03-24 22:22:32 +0400

    fix: включена вертикальная синхронизация

--- main.cpp
+++ main.cpp
@@ -59,6 +59,7 @@ int main() {
     settings.depthBits = 24;
 
     sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "Chemical-simulator", sf::State::Fullscreen, settings);
+    window.setVerticalSyncEnabled(true);
     sf::Image icon;
     if (icon.loadFromFile("assets/icon.png")) { window.setIcon(icon.getSize(), icon.getPixelsPtr()); }
 
@@ -171,9 +172,9 @@ int main() {
             renderer->camera.update(window);
             renderCounter.timer.start();
             renderer->drawShot(simulation.atoms, simulation.sim_box);
-            window.pushGLStates();
+            // window.pushGLStates();
             ImGui::SFML::Render(window);
-            window.popGLStates();
+            // window.popGLStates();
             renderCounter.timer.stop();
             renderCounter.tick(renderCounter.timer.elapsedMilliseconds());
             shotTmr = 0;
